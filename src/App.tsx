72b215c4640399ac311664c9b9f2a7c961520f8e
Ivan Venediktov <ivan.venediktov@hurtigruten.com>
2022-12-09 09:43:43 +0000

    Table headers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React from "react";
-
+import Table from "./components/Table";
+import * as mockData from "./tmp/mock-reporting-dates.json";
 const App = () => {
-	return <div>ok</div>;
+	return (
+		<div>
+			<Table
+				data={mockData}
+				headerKeys={[
+					{ name: "Company name", onClick: () => console.log("Company name") },
+					{
+						name: "Last reporting date",
+						onClick: () => console.log("Last reporting date"),
+					},
+					{
+						name: "Last reporting period",
+						onClick: () => console.log("Last reporting period"),
+					},
+					{
+						name: "Next reporting date",
+						onClick: () => console.log("Next reporting date"),
+					},
+				]}
+			/>
+		</div>
+	);
 };
 
 export default App;
--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -3,28 +3,26 @@ import { TReportItem } from "../../types";
 
 type TTable = {
 	data: Array<TReportItem>;
-	headerKeys: Array<string>;
+	headerKeys: Array<{
+		name: string;
+		onClick: () => void;
+	}>;
 };
 
-const Table = (props) => {
+const Table = (props: TTable) => {
 	const { data, headerKeys } = props;
 
 	return (
 		<table className="min-w-full text-xs">
 			<thead className="rounded-t-lg dark:bg-gray-700">
 				<tr className="text-left">
-					<th title="Company" className="p-3">
-						<button onClick={() => console.log("sort")}>Company</button>
-					</th>
-					<th title="Last reporting date" className="p-3">
-						Last reporting date
-					</th>
-					<th title="Last reporting period" className="p-3">
-						Last reporting period
-					</th>
-					<th title="Next reporting date" className="p-3">
-						Next reporting date
-					</th>
+					{headerKeys.map((headerKey, index) => (
+						<th title="Company" className="p-3" key={`header-item-${index}`}>
+							<button onClick={() => headerKey.onClick()}>
+								{headerKey.name}
+							</button>
+						</th>
+					))}
 				</tr>
 			</thead>
 		</table>
